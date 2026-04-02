8d9d84945117e7c73beea4fd637d9c7e09b4629c
soportebayzell-devs <soportebayzell@gmail.com>
2026-04-02 09:47:40 -0600

    fix: add cache config file

--- config/cache.php
+++ config/cache.php
@@ -3,16 +3,9 @@
 use Illuminate\Support\Str;
 
 return [
-
     'default' => env('CACHE_STORE', env('CACHE_DRIVER', 'redis')),
-
     'stores' => [
-
-        'array' => [
-            'driver' => 'array',
-            'serialize' => false,
-        ],
-
+        'array' => ['driver' => 'array', 'serialize' => false],
         'database' => [
             'driver' => 'database',
             'connection' => env('DB_CACHE_CONNECTION'),
@@ -20,22 +13,16 @@ return [
             'lock_connection' => env('DB_CACHE_LOCK_CONNECTION'),
             'lock_table' => env('DB_CACHE_LOCK_TABLE'),
         ],
-
         'file' => [
             'driver' => 'file',
             'path' => storage_path('framework/cache/data'),
             'lock_path' => storage_path('framework/cache/data'),
         ],
-
         'redis' => [
             'driver' => 'redis',
             'connection' => env('REDIS_CACHE_CONNECTION', 'cache'),
             'lock_connection' => env('REDIS_CACHE_LOCK_CONNECTION', 'default'),
         ],
-
     ],
-
     'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache_'),
-
 ];
-ENDOFFILE
